e1450afea815a30d403915a224633abc98ebcb35
JakubbNowek <jakubbnowek@gmail.com>
2021-04-09 18:51:17 +0200

    Rozbudowano menu ostatecznie dla jednego przypadku

--- src/Main.cpp
+++ src/Main.cpp
@@ -26,9 +26,11 @@ int main()
 
 
 	/*Menu*/
-	char choice[2] = " "; //tablica znakow zapisujaca wybor uzytkownika
+	char choice_1[2] = " "; //tablica znakow zapisujaca wybor uzytkownika
+	char choice_2[2] = " "; //tablica znakow zapisujaca wybor uzytkownika
+	char choice_3[2] = " "; //tablica znakow zapisujaca wybor uzytkownika
 	cout << endl << endl << "Witaj kliencie sortowni!" << endl;
-	while (choice[0] != '0')
+	while (choice_1[0] != '0')  /*Poczatek pierwszego switcha*/
 	{
 	
 		cout << "\nJakiego sortowanie Cie interesuje ? :" << endl;
@@ -37,26 +39,62 @@ int main()
 		cout << "  3 - IntroSort  " << endl;
 		cout << "  0 - Wypusc mnie!!!! " << endl;
 		cout << "Twoj wybor: ";
-		cin >> choice[0];
-		cout << endl << "Wybrales opcje: " << choice[0] << endl;
+		cin >> choice_1[0];
+		cout << endl << "Wybrales opcje: " << choice_1[0] << endl;
 	
-		switch (choice[0])
+		switch (choice_1[0])
 		{
-		case '1':	while (choice[0] != 'q')
+		case '1':	while (choice_2[0] != '0') /*Poczatek drugiego switcha*/							/*QuickSort*/
 					{	
 						cout << "\nDzialamy na wektorach czy tablicach?"<<endl;
 						cout << "  1 - Wektory  " << endl;
 						cout << "  2 - Tablice  " << endl;
-						cout << "  q - Cofnij  " << endl;
+						cout << "  0 - Cofnij  " << endl;
 						cout << "Twoj wybor: ";
-						cin >> choice[0];
-						cout << endl << "Wybrales opcje: " << choice[0] << endl;
+						cin >> choice_2[0];
+						cout << endl << "Wybrales opcje: " << choice_2[0] << endl << endl;
 					
-						switch (choice[0])
+						switch (choice_2[0])
 						{
-						case '1':   /*quick wektory*/cout<<"Chcialbys"<<endl; break;
-						case '2':   /*quick tablice*/cout << "Nie ma takiej opcji pajacu"<<endl; break;
-						case 'q':   cout << "Cofanie" << endl; break;
+							case '1':   /*quick wektory*/cout<<"Quick-Sort - Wektory"<<endl;
+								while (choice_3[0] != '0') /*Poczatek trzeciego switcha*/
+								{
+									cout << "\nJakie tablice chcesz sortowac?" << endl;
+									cout << "  1 - Zapelnione losowo  " << endl;
+									cout << "  2 - Posortowane czesciowo  " << endl;
+									cout << "  3 - Posortowane w odwrotnej kolejnosci  " << endl;
+									cout << "  0 - Cofnij  " << endl;
+									cout << "Twoj wybor: ";
+									cin >> choice_3[0];
+									cout << endl << "Wybrales opcje: " << choice_3[0] << endl << endl;
+
+									switch (choice_3[0])
+									{
+									case '1': cout << "Zapelnianie losowe  " << endl;
+											/* zapelnianie wektorow funkcja TabFill */
+											/* sortowanie wektorow  za pomoca quicksorta */
+										break;
+										case '2': cout << "Podaj ile elementow tabel w '%' ma byc wczesniej posortowanych [1%-100%]:  ";
+											cin >> proc; cout << endl;
+											/* zapelnianie wektorow funkcja TabFill */
+											/* sortowanie WSTĘPNE w procentach za pomoca quicksorta */
+											cout << "Tablice zostaly wstepnie posortowane w " << proc << "% kazda." << endl;
+											/*sortowanie ponowne posortowanej już tablicy za pomoca quicksorta */
+										break;
+										case '3': cout << "Sortowanie tablicy posortowanej w odwrotnej kolejnosci"<<endl;
+											/* zapelnianie wektorow funkcja TabFill */
+											/* sortowanie WSTĘPNE w odwrotnej kolejnosci (max->min) za pomoca quicksorta */
+											cout << "Tablice zostaly wstepnie posortowane by stworzyc najgorszy przypadek (malejaco)" << endl;
+											/*sortowanie ponowne posortowanych już wektorow za pomoca quicksorta */
+										break;
+										case '0': cout << "Cofanie  ";
+										break;
+										default: cout << "NIEROZPOZNANO" << endl; cin.ignore(100000, '\n');   break;
+									}
+								}
+							break;
+						case '2':   /*quick tablice*/cout << "Quick-Sort - Tablice"<<endl; break;
+						case '0':   cout << "Cofanie" << endl; break;
 						default: cout << "NIEROZPOZNANO" << endl; cin.ignore(100000, '\n');   break;
 						}
 					}
@@ -66,8 +104,6 @@ int main()
 			case '2': cout << "Podaj odleglosc :  ";
 			break;
 			case '0':  cout << endl << "Ewakuacja" << endl;
-			break;
-			case 'q':  ;
 			break;
 	
 			default: cout << "NIEROZPOZNANO" << endl; cin.ignore(100000, '\n');   break;
