a62ffff36bf664feedda3248066e86e9f06ed86b
JakubbNowek <jakubbnowek@gmail.com>
2021-04-10 13:52:19 +0200

    Wszystko dziala , teraz tylko opisac i pyk pyk

--- inc/TabFill.h
+++ inc/TabFill.h
@@ -12,7 +12,15 @@ template< typename SType>
 void  TabFill(vector<SType> toFill[], int Size) {
 	std::cout << "Wylosowano " << ROZMIAR << " tablic " << Size <<" wersowych."<< std::endl;
 	srand(time(NULL)); /*Funkcja inicjalizuj¹ca funkcjê rand(), przy ka¿dym uruchomieniu programu uzyskujemy inn¹ sekwencjê liczb losowych*/
+	
+	/*czyszczenie wektora*/
+	for (int i = 0; i < ROZMIAR; i++) { /*przechodzimy przez kolejne wektory (od wektora 0 do wektora ROZMIAR)*/
+		for (int j = 0; j < Size; j++) { /*przechodzimy przez kolejne elementy wektora (od 0 do Size)*/
 
+			toFill[i].clear();
+		}
+	}
+	/*wypelnianie wektora*/
 	for (int i = 0; i < ROZMIAR; i++) { /*przechodzimy przez kolejne wektory (od wektora 0 do wektora ROZMIAR)*/
 		for (int j = 0; j < Size; j++) { /*przechodzimy przez kolejne elementy wektora (od 0 do Size)*/
 
