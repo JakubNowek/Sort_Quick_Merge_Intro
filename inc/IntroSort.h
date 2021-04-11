dc90f11969c0186368f8ad3015361959e0f2583e
JakubbNowek <jakubbnowek@gmail.com>
2021-04-11 16:17:27 +0200

    Dzialajacy program

--- inc/IntroSort.h
+++ inc/IntroSort.h
@@ -102,7 +102,6 @@ void IntroSort(Type* Array, long N, int M)
         IntroSort(Array + i + 1, N - 1 - i, M - 1);
 }
 
-
 template <class Type>
 long Partition(Type* Array, long L, long R)
 {
@@ -112,7 +111,7 @@ long Partition(Type* Array, long L, long R)
     for (i = L, j = R - 2; ; )
     {
         for (; Array[i] < Array[R - 1]; ++i);
-        for (; j >= L && Array[j] > Array[R - 1]; --j); // >
+        for (; j >= L && Array[j] > Array[R - 1]; --j); 
         if (i < j)
             Exchange(Array, i++, j--);
         else break;
@@ -146,22 +145,6 @@ void Heap_Sort(Type* Array, long N)
     }
 }
 
-//template <class Type>
-//void Heapify(Type* Array, long i, long N)
-//{
-//    long j;
-//    while (i <= N / 2)
-//    {
-//        j = 2 * i;
-//        if (j + 1 <= N && Array[j + 1] > Array[j])
-//            j = j + 1;
-//        if (Array[i] < Array[j])
-//            Exchange(Array, i, j);
-//        else break;
-//        i = j;
-//    }
-//}
-
 template <class Type>
 void Heapify(Type* Array, long i, long N)
 {
@@ -169,28 +152,15 @@ void Heapify(Type* Array, long i, long N)
     while (i <= N / 2)
     {
         j = 2 * i;
-        if (j + 1 <= N && Array[j + 1] > Array[j]) // >
+        if (j + 1 <= N && Array[j + 1] > Array[j]) 
             j = j + 1;
-        if (Array[i] < Array[j]) // <
+        if (Array[i] < Array[j]) 
             Exchange(Array, i, j);
         else break;
         i = j;
     }
 }
 
-//template <class Type>
-//void Insertion_Sort(Type* Array, long N)
-//{
-//    long i, j;
-//    Type temp;
-//    for (i = 1; i < N; ++i)
-//    {
-//        temp = Array[i];
-//        for (j = i; j > 0 && temp < Array[j - 1]; --j)
-//            Array[j] = Array[j - 1];
-//        Array[j] = temp;
-//    }
-//}
 template <class Type>
 void Insertion_Sort(Type* Array, long N)
 {
@@ -199,7 +169,7 @@ void Insertion_Sort(Type* Array, long N)
     for (i = 1; i < N; ++i)
     {
         temp = Array[i];
-        for (j = i; j > 0 && temp < Array[j - 1]; --j) //<
+        for (j = i; j > 0 && temp < Array[j - 1]; --j) 
             Array[j] = Array[j - 1];
         Array[j] = temp;
     }
